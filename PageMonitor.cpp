3d8fe3444871082b49767b57e26dc21b72138853
HeadphoneCairn <piet.verhoeven@gmail.com>
2022-02-08 21:38:20 +0100

    F? thingies

--- PageMonitor.cpp
+++ PageMonitor.cpp
@@ -96,25 +96,44 @@ Page::LineResult PageMonitor::Line(LineFunction func, uint8_t line, uint8_t fiel
     return { 1, nullptr, Screen::inversion_none, false };
 }
 
-PSTRING(PSTR_mm_channel,           "%02d ");
-PSTRING(PSTR_mm_note_off,          "note off %s, vel %d");
-PSTRING(PSTR_mm_note_on,           "note on %s, vel %d");
-PSTRING(PSTR_mm_key_pressure,      "key pressure %s %d");
-PSTRING(PSTR_mm_cc,                "CC#%d %d");
-PSTRING(PSTR_mm_program_change,    "program change %d");
-PSTRING(PSTR_mm_channel_pressure,  "channel pressure %d");
-PSTRING(PSTR_mm_pitch_bend,        "pitch bend %lu");
+
+
+PSTRING(PSTR_mm_channel,           "%c%02d ");
+PSTRING(PSTR_mm_no_channel,        "%c-- ");
 PSTRING(PSTR_mm_unknown,           "unknown message");
+
+PSTRING(PSTR_mm_note_off,          "note off %s, vel %d");   // 0x8. 
+PSTRING(PSTR_mm_note_on,           "note on %s, vel %d");    // 0x9.
+PSTRING(PSTR_mm_key_pressure,      "key pressure %s %d");    // 0xa.
+PSTRING(PSTR_mm_cc,                "CC#%d %d");              // 0xb.
+PSTRING(PSTR_mm_program_change,    "program change %d");     // 0xc.
+PSTRING(PSTR_mm_channel_pressure,  "channel pressure %d");   // 0xd.
+PSTRING(PSTR_mm_pitch_bend,        "pitch bend %lu");        // 0xe.
+
+PSTRING(PSTR_mm_sysex_start,       "sysex start");           // 0xf0
+PSTRING(PSTR_mm_time_code,         "time code %d");          // 0xf1           
+PSTRING(PSTR_mm_song_position,     "song position %lu");     // 0xf2
+PSTRING(PSTR_mm_song_select,       "song select %d");        // 0xf3
+PSTRING(PSTR_mm_tune_request,      "tune request");          // 0xf6
+PSTRING(PSTR_mm_sysex_end,         "sysex end");             // 0xf7
+
+PSTRING(PSTR_mm_timing_clock,      "timing clock");          // 0xf8
+PSTRING(PSTR_mm_start,             "start");                 // 0xfa
+PSTRING(PSTR_mm_continue,          "continue");              // 0xfb
+PSTRING(PSTR_mm_stop,              "stop");                  // 0xfc
+PSTRING(PSTR_mm_active_sensing,    "active sensing");        // 0xfe
+PSTRING(PSTR_mm_system_reset,      "reset");                 // 0xff         
 
 /*
 TODO 
 - snprintf! to prevent buffer overrun
-- note instead of key number!!!  
+x note instead of key number!!!  
 - type of CC: mod/volume/bank select/...
-- all F.. thingies
+x all F.. thingies
 - sysex, how to show
 - running status ???
 - implemen settings and filters
+- check MSB/LSB order of pitch bend and song position
 */
 
 Page::LineResult PageMonitor::LineDecode(const midi_msg_t& msg)
@@ -131,15 +150,16 @@ Page::LineResult PageMonitor::LineDecode(const midi_msg_t& msg)
 
   // --- Add the decode message to text (if there is a message) ---
   if (e.m_event != 0) {
-    // Add (I)nput or (O)utput
-    text[0] = msg.input ? 'I' : 'O';
-    text++;
-    // Add text for messages that are channel specific
+    // Add (I)nput/(O)utput and channel
+    char in_or_out = msg.input ? 'I' : 'O';
     if (e.m_event >= 0x8 && e.m_event <= 0xe) {
-      // Add the channel number
       const uint8_t channel = e.m_data[0] & 0x0f;
-      text += sprintf(text, GetPString(PSTR_mm_channel), channel + 1);
-      // Add the event text
+      text += sprintf(text, GetPString(PSTR_mm_channel), in_or_out, channel + 1);
+    } else {
+      text += sprintf(text, GetPString(PSTR_mm_no_channel), in_or_out);
+    }
+    // Add event text for messages that are channel specific
+    if (e.m_event >= 0x8 && e.m_event <= 0xe) {
       switch (e.m_event)
       { 
         case 0x8:
@@ -164,15 +184,59 @@ Page::LineResult PageMonitor::LineDecode(const midi_msg_t& msg)
           sprintf(text, GetPString(PSTR_mm_channel_pressure), e.m_data[1]);
           break;
         case 0xe:
-          sprintf(text, GetPString(PSTR_mm_pitch_bend), static_cast<unsigned long>(e.m_data[2]) * 128ul + static_cast<unsigned long>(e.m_data[1]));
+          sprintf(text, GetPString(PSTR_mm_pitch_bend), static_cast<unsigned long>(e.m_data[2]) * 128ul + static_cast<unsigned long>(e.m_data[1])); // LSB, MSB
           break;
         default:
           strcpy(text, GetPString(PSTR_mm_unknown));
           break;
       }
-    } else if (e.m_event == 0xf) {
-      strcpy(text, "sysex enzo");
-    } else {
+    }
+    // Add event text for messages that are channel independent
+    else if (e.m_event == 0xf) {
+      switch (e.m_data[0]) {
+        case 0xf0:
+          sprintf(text, GetPString(PSTR_mm_sysex_start));
+          break;
+        case 0xf1:
+          sprintf(text, GetPString(PSTR_mm_time_code), e.m_data[1]);
+          break;
+        case 0xf2:
+          sprintf(text, GetPString(PSTR_mm_song_position), static_cast<unsigned long>(e.m_data[1]) * 128ul + static_cast<unsigned long>(e.m_data[2]));  // MSB, LSB
+          break;
+        case 0xf3:
+          sprintf(text, GetPString(PSTR_mm_song_select), e.m_data[1]);
+          break;
+        case 0xf6:
+          sprintf(text, GetPString(PSTR_mm_tune_request));
+          break;
+        case 0xf7:
+          sprintf(text, GetPString(PSTR_mm_sysex_end));
+          break;
+        case 0xf8:
+          sprintf(text, GetPString(PSTR_mm_timing_clock));
+          break;
+        case 0xfa:
+          sprintf(text, GetPString(PSTR_mm_start));
+          break;
+        case 0xfb:
+          sprintf(text, GetPString(PSTR_mm_continue));
+          break;
+        case 0xfc:
+          sprintf(text, GetPString(PSTR_mm_stop));
+          break;
+        case 0xfe:
+          sprintf(text, GetPString(PSTR_mm_active_sensing));
+          break;
+        case 0xff:
+          sprintf(text, GetPString(PSTR_mm_system_reset));
+          break;
+        default:
+          strcpy(text, GetPString(PSTR_mm_unknown));
+          break;
+      }
+    }
+    // Unknown message
+    else {
       strcpy(text, GetPString(PSTR_mm_unknown));
     }
   }
