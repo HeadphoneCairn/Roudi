c2cfead525aa74bf7538933e9f85862a53909efe
HeadphoneCairn <piet.verhoeven@gmail.com>
2022-02-08 20:52:06 +0100

    Convert note number to name

--- PageMonitor.cpp
+++ PageMonitor.cpp
@@ -97,9 +97,9 @@ Page::LineResult PageMonitor::Line(LineFunction func, uint8_t line, uint8_t fiel
 }
 
 PSTRING(PSTR_mm_channel,           "%02d ");
-PSTRING(PSTR_mm_note_off,          "note off %d, vel %d");
-PSTRING(PSTR_mm_note_on,           "note on %d, vel %d");
-PSTRING(PSTR_mm_key_pressure,      "key pressure %d %d");
+PSTRING(PSTR_mm_note_off,          "note off %s, vel %d");
+PSTRING(PSTR_mm_note_on,           "note on %s, vel %d");
+PSTRING(PSTR_mm_key_pressure,      "key pressure %s %d");
 PSTRING(PSTR_mm_cc,                "CC#%d %d");
 PSTRING(PSTR_mm_program_change,    "program change %d");
 PSTRING(PSTR_mm_channel_pressure,  "channel pressure %d");
@@ -119,26 +119,40 @@ TODO
 
 Page::LineResult PageMonitor::LineDecode(const midi_msg_t& msg)
 {
+  // --- Shortcut to the event ---
   const midi_event_t& e = msg.event;
+  // --- The output text ---
   char* text = Screen::buffer;
   text[0] = 0;
+  // --- Temporary storage for note name ---
+  const uint8_t max_note_name_len = 4; // max note name string length = 4 => C#-1
+  char note_name[max_note_name_len + 1];
+  note_name[max_note_name_len] = 0;
 
+  // --- Add the decode message to text (if there is a message) ---
   if (e.m_event != 0) {
+    // Add (I)nput or (O)utput
     text[0] = msg.input ? 'I' : 'O';
     text++;
+    // Add text for messages that are channel specific
     if (e.m_event >= 0x8 && e.m_event <= 0xe) {
+      // Add the channel number
       const uint8_t channel = e.m_data[0] & 0x0f;
       text += sprintf(text, GetPString(PSTR_mm_channel), channel + 1);
+      // Add the event text
       switch (e.m_event)
       { 
         case 0x8:
-          sprintf(text, GetPString(PSTR_mm_note_off), e.m_data[1], e.m_data[2]);
+          strncpy(note_name, GetNoteName(e.m_data[1]), max_note_name_len);
+          sprintf(text, GetPString(PSTR_mm_note_off), note_name, e.m_data[2]);
           break;
         case 0x9:
-          sprintf(text, GetPString(PSTR_mm_note_on), e.m_data[1], e.m_data[2]);
+          strncpy(note_name, GetNoteName(e.m_data[1]), max_note_name_len);
+          sprintf(text, GetPString(PSTR_mm_note_on), note_name, e.m_data[2]);
           break;
         case 0xa:
-          sprintf(text, GetPString(PSTR_mm_key_pressure), e.m_data[1], e.m_data[2]);
+          strncpy(note_name, GetNoteName(e.m_data[1]), max_note_name_len);
+          sprintf(text, GetPString(PSTR_mm_key_pressure), note_name, e.m_data[2]);
           break;
         case 0xb:
           sprintf(text, GetPString(PSTR_mm_cc), e.m_data[1], e.m_data[2]);
