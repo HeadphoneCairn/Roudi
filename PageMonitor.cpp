12945cb42c2ab94f5d2c807ecb11a124417eb548
HeadphoneCairn <piet.verhoeven@gmail.com>
2022-02-20 13:37:26 +0100

    More cleanup

--- PageMonitor.cpp
+++ PageMonitor.cpp
@@ -29,6 +29,7 @@ namespace {
     page_monitor->m_messages.Push({event, 1});
     SetRedrawNext();
   }
+
   void ListenOut(const midi_event_t& event, void* data)
   {
     PageMonitor* page_monitor = static_cast<PageMonitor*>(data);
@@ -85,151 +86,153 @@ Page::LineResult PageMonitor::Line(LineFunction func, uint8_t line, uint8_t fiel
     return { 1, nullptr, Screen::inversion_none, false };
 }
 
+namespace { 
 
+  // List of more or less standard uses of CCs.
+  // Max 10 characters "0123456789"
 
-// List of more or less standard uses of CCs.
-// Max 10 characters "0123456789"
+  PSTRING(PSTR_cc_0,   "bank selec");
+  PSTRING(PSTR_cc_1,   "mod wheel" );
+  PSTRING(PSTR_cc_2,   "breath"    );
+  PSTRING(PSTR_cc_4,   "foot pedal");
+  PSTRING(PSTR_cc_5,   "portamento");
+  PSTRING(PSTR_cc_6,   "data entry");
+  PSTRING(PSTR_cc_7,   "volume"    );
+  PSTRING(PSTR_cc_8,   "balance"   );
+  PSTRING(PSTR_cc_10,  "pan"       );
+  PSTRING(PSTR_cc_11,  "expression");
+  PSTRING(PSTR_cc_12,  "effect 1"  );
+  PSTRING(PSTR_cc_13,  "effect 2"  );
 
-PSTRING(PSTR_cc_0,   "bank selec");
-PSTRING(PSTR_cc_1,   "mod wheel" );
-PSTRING(PSTR_cc_2,   "breath"    );
-PSTRING(PSTR_cc_4,   "foot pedal");
-PSTRING(PSTR_cc_5,   "portamento");
-PSTRING(PSTR_cc_6,   "data entry");
-PSTRING(PSTR_cc_7,   "volume"    );
-PSTRING(PSTR_cc_8,   "balance"   );
-PSTRING(PSTR_cc_10,  "pan"       );
-PSTRING(PSTR_cc_11,  "expression");
-PSTRING(PSTR_cc_12,  "effect 1"  );
-PSTRING(PSTR_cc_13,  "effect 2"  );
+  PSTRING(PSTR_cc_64,  "sustain p.");
+  PSTRING(PSTR_cc_65,  "portamento");
+  PSTRING(PSTR_cc_66,  "sostenuto" );
+  PSTRING(PSTR_cc_67,  "soft pedal");
+  PSTRING(PSTR_cc_68,  "legato"    );
+  PSTRING(PSTR_cc_69,  "hold pedal");
+  PSTRING(PSTR_cc_70,  "sound ct1" );
+  PSTRING(PSTR_cc_71,  "resonance" );
+  PSTRING(PSTR_cc_72,  "release"   );
+  PSTRING(PSTR_cc_73,  "attack"    );
+  PSTRING(PSTR_cc_74,  "cutoff"    );
+  PSTRING(PSTR_cc_75,  "sound ct6" );
+  PSTRING(PSTR_cc_76,  "Sound ct7" );
+  PSTRING(PSTR_cc_77,  "Sound ct8" );
+  PSTRING(PSTR_cc_78,  "Sound ct9" );
+  PSTRING(PSTR_cc_79,  "Sound ct10");
+  PSTRING(PSTR_cc_80,  "decay"     );
+  PSTRING(PSTR_cc_91,  "reverb"    );
+  PSTRING(PSTR_cc_92,  "tremolo"   );
+  PSTRING(PSTR_cc_93,  "chorus"    );
+  PSTRING(PSTR_cc_94,  "detune"    );
+  PSTRING(PSTR_cc_95,  "phaser"    );
+  PSTRING(PSTR_cc_96,  "data +"    );
+  PSTRING(PSTR_cc_97,  "data -"    );
 
-PSTRING(PSTR_cc_64,  "sustain p.");
-PSTRING(PSTR_cc_65,  "portamento");
-PSTRING(PSTR_cc_66,  "sostenuto" );
-PSTRING(PSTR_cc_67,  "soft pedal");
-PSTRING(PSTR_cc_68,  "legato"    );
-PSTRING(PSTR_cc_69,  "hold pedal");
-PSTRING(PSTR_cc_70,  "sound ct1" );
-PSTRING(PSTR_cc_71,  "resonance" );
-PSTRING(PSTR_cc_72,  "release"   );
-PSTRING(PSTR_cc_73,  "attack"    );
-PSTRING(PSTR_cc_74,  "cutoff"    );
-PSTRING(PSTR_cc_75,  "sound ct6" );
-PSTRING(PSTR_cc_76,  "Sound ct7" );
-PSTRING(PSTR_cc_77,  "Sound ct8" );
-PSTRING(PSTR_cc_78,  "Sound ct9" );
-PSTRING(PSTR_cc_79,  "Sound ct10");
-PSTRING(PSTR_cc_80,  "decay"     );
-PSTRING(PSTR_cc_91,  "reverb"    );
-PSTRING(PSTR_cc_92,  "tremolo"   );
-PSTRING(PSTR_cc_93,  "chorus"    );
-PSTRING(PSTR_cc_94,  "detune"    );
-PSTRING(PSTR_cc_95,  "phaser"    );
-PSTRING(PSTR_cc_96,  "data +"    );
-PSTRING(PSTR_cc_97,  "data -"    );
+  PSTRING(PSTR_cc_120, "sounds off");
+  PSTRING(PSTR_cc_121, "ctrlrs off");
+  PSTRING(PSTR_cc_122, "local"     );
+  PSTRING(PSTR_cc_123, "notes off" );
+  PSTRING(PSTR_cc_124, "omni off"  );
+  PSTRING(PSTR_cc_125, "omni on"   );
+  PSTRING(PSTR_cc_126, "mono"      );
+  PSTRING(PSTR_cc_127, "poly"      );
 
-PSTRING(PSTR_cc_120, "sounds off");
-PSTRING(PSTR_cc_121, "ctrlrs off");
-PSTRING(PSTR_cc_122, "local"     );
-PSTRING(PSTR_cc_123, "notes off" );
-PSTRING(PSTR_cc_124, "omni off"  );
-PSTRING(PSTR_cc_125, "omni on"   );
-PSTRING(PSTR_cc_126, "mono"      );
-PSTRING(PSTR_cc_127, "poly"      );
+  PSTRING(PSTR_cc_default, "-");
 
-PSTRING(PSTR_cc_default, "-");
-
-// Convert cc number to cc "standard" name
-// This function uses almost 1,000 bytes of my 28,672 bytes of program storage space!
-static const char* GetCCName(uint8_t cc)
-{
-  switch(cc) {
-    case   0 + 32:
-    case   0: return GetPString(PSTR_cc_0);
-    case   1 + 32:
-    case   1: return GetPString(PSTR_cc_1);
-    case   2 + 32:
-    case   2: return GetPString(PSTR_cc_2);
-    case   4 + 32:
-    case   4: return GetPString(PSTR_cc_4);
-    case   5 + 32:
-    case   5: return GetPString(PSTR_cc_5);
-    case   6 + 32:
-    case   6: return GetPString(PSTR_cc_6);
-    case   7 + 32:
-    case   7: return GetPString(PSTR_cc_7);
-    case   8 + 32:
-    case   8: return GetPString(PSTR_cc_8);
-    case  10 + 32:
-    case  10: return GetPString(PSTR_cc_10);
-    case  11 + 32:
-    case  11: return GetPString(PSTR_cc_11);
-    case  12 + 32:
-    case  12: return GetPString(PSTR_cc_12);
-    case  13 + 32:
-    case  13: return GetPString(PSTR_cc_13);
-    case  64: return GetPString(PSTR_cc_64);
-    case  65: return GetPString(PSTR_cc_65);
-    case  66: return GetPString(PSTR_cc_66);
-    case  67: return GetPString(PSTR_cc_67);
-    case  68: return GetPString(PSTR_cc_68);
-    case  69: return GetPString(PSTR_cc_69);
-    case  70: return GetPString(PSTR_cc_70);
-    case  71: return GetPString(PSTR_cc_71);
-    case  72: return GetPString(PSTR_cc_72);
-    case  73: return GetPString(PSTR_cc_73);
-    case  74: return GetPString(PSTR_cc_74);
-    case  75: return GetPString(PSTR_cc_75);
-    case  76: return GetPString(PSTR_cc_76);
-    case  77: return GetPString(PSTR_cc_77);
-    case  78: return GetPString(PSTR_cc_78);
-    case  79: return GetPString(PSTR_cc_79);
-    case  80: return GetPString(PSTR_cc_80);
-    case  91: return GetPString(PSTR_cc_91);
-    case  92: return GetPString(PSTR_cc_92);
-    case  93: return GetPString(PSTR_cc_93);
-    case  94: return GetPString(PSTR_cc_94);
-    case  95: return GetPString(PSTR_cc_95);
-    case  96: return GetPString(PSTR_cc_96);
-    case  97: return GetPString(PSTR_cc_97);
-    case 120: return GetPString(PSTR_cc_120);
-    case 121: return GetPString(PSTR_cc_121);
-    case 122: return GetPString(PSTR_cc_122);
-    case 123: return GetPString(PSTR_cc_123);
-    case 124: return GetPString(PSTR_cc_124);
-    case 125: return GetPString(PSTR_cc_125);
-    case 126: return GetPString(PSTR_cc_126);
-    case 127: return GetPString(PSTR_cc_127);
-    default:  return GetPString(PSTR_cc_default);
+  // Convert cc number to cc "standard" name
+  // This function uses almost 1,000 bytes of my 28,672 bytes of program storage space!
+  const char* GetCCName(uint8_t cc)
+  {
+    switch(cc) {
+      case   0 + 32:
+      case   0: return GetPString(PSTR_cc_0);
+      case   1 + 32:
+      case   1: return GetPString(PSTR_cc_1);
+      case   2 + 32:
+      case   2: return GetPString(PSTR_cc_2);
+      case   4 + 32:
+      case   4: return GetPString(PSTR_cc_4);
+      case   5 + 32:
+      case   5: return GetPString(PSTR_cc_5);
+      case   6 + 32:
+      case   6: return GetPString(PSTR_cc_6);
+      case   7 + 32:
+      case   7: return GetPString(PSTR_cc_7);
+      case   8 + 32:
+      case   8: return GetPString(PSTR_cc_8);
+      case  10 + 32:
+      case  10: return GetPString(PSTR_cc_10);
+      case  11 + 32:
+      case  11: return GetPString(PSTR_cc_11);
+      case  12 + 32:
+      case  12: return GetPString(PSTR_cc_12);
+      case  13 + 32:
+      case  13: return GetPString(PSTR_cc_13);
+      case  64: return GetPString(PSTR_cc_64);
+      case  65: return GetPString(PSTR_cc_65);
+      case  66: return GetPString(PSTR_cc_66);
+      case  67: return GetPString(PSTR_cc_67);
+      case  68: return GetPString(PSTR_cc_68);
+      case  69: return GetPString(PSTR_cc_69);
+      case  70: return GetPString(PSTR_cc_70);
+      case  71: return GetPString(PSTR_cc_71);
+      case  72: return GetPString(PSTR_cc_72);
+      case  73: return GetPString(PSTR_cc_73);
+      case  74: return GetPString(PSTR_cc_74);
+      case  75: return GetPString(PSTR_cc_75);
+      case  76: return GetPString(PSTR_cc_76);
+      case  77: return GetPString(PSTR_cc_77);
+      case  78: return GetPString(PSTR_cc_78);
+      case  79: return GetPString(PSTR_cc_79);
+      case  80: return GetPString(PSTR_cc_80);
+      case  91: return GetPString(PSTR_cc_91);
+      case  92: return GetPString(PSTR_cc_92);
+      case  93: return GetPString(PSTR_cc_93);
+      case  94: return GetPString(PSTR_cc_94);
+      case  95: return GetPString(PSTR_cc_95);
+      case  96: return GetPString(PSTR_cc_96);
+      case  97: return GetPString(PSTR_cc_97);
+      case 120: return GetPString(PSTR_cc_120);
+      case 121: return GetPString(PSTR_cc_121);
+      case 122: return GetPString(PSTR_cc_122);
+      case 123: return GetPString(PSTR_cc_123);
+      case 124: return GetPString(PSTR_cc_124);
+      case 125: return GetPString(PSTR_cc_125);
+      case 126: return GetPString(PSTR_cc_126);
+      case 127: return GetPString(PSTR_cc_127);
+      default:  return GetPString(PSTR_cc_default);
+    }
   }
+
+
+  PSTRING(PSTR_mm_channel,           "%c%02d ");
+  PSTRING(PSTR_mm_no_channel,        "%c-- ");
+  PSTRING(PSTR_mm_unknown,           "unknown message");
+
+  PSTRING(PSTR_mm_note_off,          "note off %s, vel %d");   // 0x8. 
+  PSTRING(PSTR_mm_note_on,           "note on %s, vel %d");    // 0x9.
+  PSTRING(PSTR_mm_key_pressure,      "key pressure %s %d");    // 0xa.
+  PSTRING(PSTR_mm_cc,                "CC#%d %s %d");           // 0xb.
+  PSTRING(PSTR_mm_program_change,    "program change %d");     // 0xc.
+  PSTRING(PSTR_mm_channel_pressure,  "channel pressure %d");   // 0xd.
+  PSTRING(PSTR_mm_pitch_bend,        "pitch bend %ld");        // 0xe.
+
+  PSTRING(PSTR_mm_sysex_start,       "sysex start");           // 0xf0
+  PSTRING(PSTR_mm_time_code,         "time code %d");          // 0xf1           
+  PSTRING(PSTR_mm_song_position,     "song position %lu");     // 0xf2
+  PSTRING(PSTR_mm_song_select,       "song select %d");        // 0xf3
+  PSTRING(PSTR_mm_tune_request,      "tune request");          // 0xf6
+  PSTRING(PSTR_mm_sysex_end,         "sysex end");             // 0xf7
+
+  PSTRING(PSTR_mm_timing_clock,      "timing clock");          // 0xf8
+  PSTRING(PSTR_mm_start,             "start");                 // 0xfa
+  PSTRING(PSTR_mm_continue,          "continue");              // 0xfb
+  PSTRING(PSTR_mm_stop,              "stop");                  // 0xfc
+  PSTRING(PSTR_mm_active_sensing,    "active sensing");        // 0xfe
+  PSTRING(PSTR_mm_system_reset,      "reset");                 // 0xff         
+
 }
-
-
-PSTRING(PSTR_mm_channel,           "%c%02d ");
-PSTRING(PSTR_mm_no_channel,        "%c-- ");
-PSTRING(PSTR_mm_unknown,           "unknown message");
-
-PSTRING(PSTR_mm_note_off,          "note off %s, vel %d");   // 0x8. 
-PSTRING(PSTR_mm_note_on,           "note on %s, vel %d");    // 0x9.
-PSTRING(PSTR_mm_key_pressure,      "key pressure %s %d");    // 0xa.
-PSTRING(PSTR_mm_cc,                "CC#%d %s %d");           // 0xb.
-PSTRING(PSTR_mm_program_change,    "program change %d");     // 0xc.
-PSTRING(PSTR_mm_channel_pressure,  "channel pressure %d");   // 0xd.
-PSTRING(PSTR_mm_pitch_bend,        "pitch bend %ld");        // 0xe.
-
-PSTRING(PSTR_mm_sysex_start,       "sysex start");           // 0xf0
-PSTRING(PSTR_mm_time_code,         "time code %d");          // 0xf1           
-PSTRING(PSTR_mm_song_position,     "song position %lu");     // 0xf2
-PSTRING(PSTR_mm_song_select,       "song select %d");        // 0xf3
-PSTRING(PSTR_mm_tune_request,      "tune request");          // 0xf6
-PSTRING(PSTR_mm_sysex_end,         "sysex end");             // 0xf7
-
-PSTRING(PSTR_mm_timing_clock,      "timing clock");          // 0xf8
-PSTRING(PSTR_mm_start,             "start");                 // 0xfa
-PSTRING(PSTR_mm_continue,          "continue");              // 0xfb
-PSTRING(PSTR_mm_stop,              "stop");                  // 0xfc
-PSTRING(PSTR_mm_active_sensing,    "active sensing");        // 0xfe
-PSTRING(PSTR_mm_system_reset,      "reset");                 // 0xff         
 
 /*
 TODO 
