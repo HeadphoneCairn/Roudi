9a553d01e472d52805765542be8e1886919b89b9
HeadphoneCairn <piet.verhoeven@gmail.com>
2022-04-01 21:28:45 +0200

    Another define

--- PageAbout.cpp
+++ PageAbout.cpp
@@ -5,6 +5,7 @@
 #include "MemoryFree.h"
 #include "MidiProcessing.h"
 #include "Pages.h"
+#include "Roudi.h" // for ENABLE_MEMORY_STATUS
 
 
 PSTRING(PSTR_page_about, " ABOUT ROUDI "); 
@@ -27,7 +28,6 @@ PSTRING(PSTR_about_13, " A + B: toggle utils");
 PSTRING(PSTR_about_14, " up, down: switch fields");
 PSTRING(PSTR_about_15, " left, right: modify");
 PSTRING(PSTR_about_16, "");
-PSTRING(PSTR_about_17, "STATUS:");
 
 /*
 To add:
@@ -52,17 +52,17 @@ PTABLE(PTAB_about_text,
        PSTR_about_14,
        PSTR_about_15,
        PSTR_about_16,
-       PSTR_about_17,
        );
 
-
-
+#ifdef ENABLE_MEMORY_STATUS
+PSTRING(PSTR_about_status,       "STATUS:");
 PSTRING(PSTR_about_free_heap,    " free heap mem: %d bytes");
 PSTRING(PSTR_about_min_stack,    " min stack mem: %d bytes");
 PSTRING(PSTR_about_heap_frag_ok, " heap fragments: %d");
 PSTRING(PSTR_about_heap_frag_nok," heap fragments: %d !!!");
 PSTRING(PSTR_about_biggest_page, " biggest page: %d bytes");
 PSTRING(PSTR_about_sizeof,       " > %s");
+#endif
 
 
 uint8_t PageAbout::m_selected_line = 0;
@@ -74,7 +74,11 @@ PageAbout::PageAbout(): Page()
 
 void PageAbout::OnStart(uint8_t)
 {
-  SetNumberOfLines(PTAB_about_text_size + 5, m_selected_line, 0, m_first_line);
+  SetNumberOfLines(PTAB_about_text_size 
+#ifdef ENABLE_MEMORY_STATUS
+  + 6
+#endif
+  , m_selected_line, 0, m_first_line);
   SetMidiConfiguration(); // TODO
 }
 
@@ -93,19 +97,25 @@ Page::LineResult PageAbout::Line(LineFunction func, uint8_t line, uint8_t field)
 {
   if (line < PTAB_about_text_size)
     return {1, GetPStringFromPTable(PTAB_about_text, line), Screen::inversion_all, false};
+#ifdef ENABLE_MEMORY_STATUS
   else if (line == PTAB_about_text_size)
-    snprintf(Screen::buffer, sizeof(Screen::buffer), GetPString(PSTR_about_free_heap), getFreeMemory()-getMallocMargin());
+    return TextLine(func, PSTR_about_status);
   else if (line == PTAB_about_text_size + 1)
-    snprintf(Screen::buffer, sizeof(Screen::buffer), GetPString(PSTR_about_min_stack), getMallocMargin());
+    snprintf(Screen::buffer, sizeof(Screen::buffer), GetPString(PSTR_about_free_heap), getFreeMemory()-getMallocMargin());
   else if (line == PTAB_about_text_size + 2)
+    snprintf(Screen::buffer, sizeof(Screen::buffer), GetPString(PSTR_about_min_stack), getMallocMargin());
+  else if (line == PTAB_about_text_size + 3)
     snprintf(Screen::buffer, sizeof(Screen::buffer), 
             GetPString((getNumberOfHeapFragments() == 1) ? PSTR_about_heap_frag_ok: PSTR_about_heap_frag_nok), 
             getNumberOfHeapFragments());
-  else if (line == PTAB_about_text_size + 3)
+  else if (line == PTAB_about_text_size + 4)
     snprintf(Screen::buffer, sizeof(Screen::buffer), GetPString(PSTR_about_biggest_page), Pages::GetBiggestPageUsage());
   else
     snprintf(Screen::buffer, sizeof(Screen::buffer), GetPString(PSTR_about_sizeof), Pages::GetPageUsage());
   return {1, Screen::buffer, Screen::inversion_all, false};
+#else
+  return DefaultLine(func);
+#endif
 }
 
 void PageAbout::SetMidiConfiguration()
--- Roudi.h
+++ Roudi.h
@@ -2,16 +2,29 @@
 
 // --- Some global defines that can be used to configure Roudi ---
 
-// Enables showing of Headphone Cairn logo at startup. Should be on on release.
+// Enables showing of Headphone Cairn logo at startup. 
+// Should be on on release.
 //#define ENABLE_LOGO
-// Enables debug printing and beeping. Should be off on release.
+
+// Enables showing the memory status in the About Page
+// Should be off on release.
+#define ENABLE_MEMORY_STATUS
+
+// Enables debug printing and beeping.
+// Should be off on release.
 #define ENABLE_DEBUG
-// Beeps every time that the eeprom is written to. Should be off on release.
+
+// Beeps every time that the eeprom is written to.
 // Only works when ENABLE_DEBUG is on!
+// Should be off on release.
 #define ENABLE_WRITE_BEEP
-// When eeprom is initialized, writes favorite channels names. Should be off on release.
+
+// When eeprom is initialized, writes favorite channels names.
+// Should be off on release.
 #define ENABLE_DEFAULT_CHANNEL_NAMES
-// Add the raw unfiltered input messages to the monitor. Should be off on release.
+
+// Add the raw unfiltered input messages to the monitor.
+// Should be off on release.
 //#define ENABLE_MONITOR_RAW
 
 
